9e8c2f2b645680a2804ac787c3a9a33a36628629
김다은 <softeun92@naver.com>
2021-11-13 12:19:16 +0900

    조건문연습01 if/else if/ eles체험

--- app/src/main/java/com/nepplus/kotlengrammer/MainActivity.kt
+++ app/src/main/java/com/nepplus/kotlengrammer/MainActivity.kt
@@ -34,10 +34,30 @@ class MainActivity : AppCompatActivity() {
             val myBrithYear = 1974
             var myheight = 174.5
 
-            myheight = 190
+
 
             Log.d("변수연습02", myName)
         }
+
+        btnCondition01.setOnClickListener {
+//            조건문 번튼 눌리면면 실횅 코드
+            val userAge = 10
+//            나이가 이상인가요> 로그로 성인입니다. 문장 출력
+            if(userAge >=20 ){
+
+                Log.d("조건문01","성인입니다.")
+
+            }
+            else if(userAge >= 17){
+                Log.d("조건문01", "고등학생입니다.")
+            }
+            else if(userAge >= 14){
+                Log.d("조건문01", "중학생입니다.")
+            }
+            else{
+                Log.d("조건문01", "초등학생이거나, 미취학생입니다.")
+            }
+       }
 
 
     }
